6aece8430bdc0139a6ad73d57158b5e9ec552214
NamaWorks <pedronayamartinez@hotmail.com>
2025-03-28 19:03:30 +0100

    prepare reservationProvider layout

--- src/components/ui/BookNowButton/BookNowButton.tsx
+++ src/components/ui/BookNowButton/BookNowButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { oswald } from '@/utils/fonts/fonts'
 import React from 'react'
 
--- src/utils/providers/ReservationProvider.tsx
+++ src/utils/providers/ReservationProvider.tsx
@@ -3,42 +3,83 @@
 import { ReactNode, useContext, useEffect } from "react";
 import { CalendarContext } from "../contexts/contexts";
 import { CalendarContextInterface } from "../interfaces/interfaces";
+import { Poppins } from "next/font/google";
+import BookNowButton from "@/components/ui/BookNowButton/BookNowButton";
+import { latoSans, oswald } from "../fonts/fonts";
 
 const ReservationProvider = ({children}: {children: ReactNode}) => {
 
   const { dates, setDates, chosenDate, setChosenDate, reservationsOpen, setReservationsOpen } = useContext(CalendarContext) as CalendarContextInterface
 
+  const hours = ['9am', '11am', '3pm', '5pm', '7pm']
+
   useEffect(()=>{
-    setReservationsOpen(false)
+    setReservationsOpen(true)
   },[setReservationsOpen])
 
   return (
     <>
     {
     reservationsOpen &&
-  <div className="fixed z-99 bg-[#D9D9D9] rounded-[5px] p-3 top-[50%] right-3">
-    <h3>Book a Photoshooth - 2h</h3>
+  <div className={`fixed z-99 bg-[#D9D9D9] rounded-[5px] p-3 bottom-3 right-3`}>
+    <h3 className={`${oswald.className} font-medium text-[24px] uppercase`}>Book a Photoshooth - 2h</h3>
     <div>
+
+
+      {/* from here the toggle for dates */}
+      <div className={`uppercase w-full bg-[#ffffff] p-3`}>
       {
-        chosenDate &&
+        chosenDate ?
         <>
-          <div>
             {chosenDate.toString()}
-          </div>
+        </>
+        :
+        <>
+          select date
         </>
       }
+      </div>
+
+      {/* from here the hours */}
       <div>
         {
           dates.map((date, i)=>{
             return (
               <>
-                <div>{date.toString()}</div>
+                <div key={date.toString() + i}>{date.toString()}</div>
               </>
             )
           })
         }
       </div>
+
+      <div>
+        {
+          hours.map((hour)=>{
+            return (
+              <>
+                <div key={hour} className="">
+                  <p>{hour}</p>
+                </div>
+              </>
+            )
+          })
+        }
+      </div>
+
     </div>
+    
+    {/* from here the form with the details */}
+    <form action="" className="flex flex-col items-end gap-3">
+      <input className={`w-full ${latoSans.className} text-[18px] border-solid border-[#000] border-b-1 border-r-0 border-l-0 boder-t-0 `} type="text" placeholder="First Name *" required />
+      <input className={`w-full ${latoSans.className} text-[18px] border-solid border-[#000] border-b-1 border-r-0 border-l-0 boder-t-0 `} type="text" placeholder="Last Name *" required />
+      <input className={`w-full ${latoSans.className} text-[18px] border-solid border-[#000] border-b-1 border-r-0 border-l-0 boder-t-0 `} type="email" placeholder="Email *" required />
+      <input className={`w-full ${latoSans.className} text-[18px] border-solid border-[#000] border-b-1 border-r-0 border-l-0 boder-t-0 `} type="tel" placeholder="Phone Number" />
+      <button className='bg-[#ffffff] rounded-[5px] p-3 px-12 justify-self-end w-60 cursor-pointer'>
+        <p className={`${oswald.className} uppercase font-light`}>Book <span className='font-semibold'>Now</span></p>
+      </button>
+    </form>
+
   </div>
 }
     {children}
